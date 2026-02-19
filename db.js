494f40f9c9fd6ffdf358b8eb4d5d8b2973cd2164
Alitedigitals2007 <161524740+Alitedigitals2007@users.noreply.github.com>
2026-02-19 18:56:29 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,17 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Required for most cloud DB providers
-  }
+    rejectUnauthorized: false // Required for Neon
+  },
+  keepAlive: true
 });
+
+// This will log the specific error to your Vercel "Functions" log
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+});
+
+module.exports = pool;
