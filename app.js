aada1fb1a7d34c0f99a22f43a3991e2a3a1225a0
Alitedigitals2007 <161524740+Alitedigitals2007@users.noreply.github.com>
2026-02-19 19:01:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const db = require('./db');
 // 1. MIDDLEWARE SETUP
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public')); // Crucial for serving uploaded images
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: process.env.SESSION_SECRET || 'alite_secret_key',
     resave: false,
